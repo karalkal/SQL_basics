0e05f2a1875c5bd98da889c2a1e8936cb9ae1d1e
karalkal <karalkal@yahoo.co.uk>
2022-01-19 01:19:00 +0000

    added short exercise with queries

---
+++ people->cities->countries/02_INSERT_capitals_as_cities+add_people.sql
@@ -0,0 +1,92 @@
+INSERT INTO cities (name, country_id, population, is_capital)
+    SELECT name, country_id, population, True from capitals;
+    
+
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Sang', 'Reed', 28908390161, 44);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Lance', 'Adair', 12297804584, 45);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Marilyn', 'Fontenot', 13843667088, 43);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Jodee', 'Duff', 92371848899, 43);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Miguelina', 'Drake', 54237519957, 42);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Leena', 'Casas', 48590492252, 38);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Alethia', 'Prince', 31825922863, 45);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Celsa', 'Lilly', 91086181486, 44);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Alpha', 'Thurman', 59777860378, 45);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Shaquita', 'Moreau', 18869122653, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Norah', 'Comstock', 42835021093, 44);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Rhett', 'Ventura', 85949387864, 43);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Adelaida', 'Billiot', 46825667430, 38);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Guadalupe', 'Shannon', 59906478300, 45);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Theresa', 'Locklear', 86551447724, 43);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Ettie', 'Elam', 47434158112, 38);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Ja', 'Garland', 90104313321, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Amberly', 'Moses', 50644564582, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Zulma', 'Hawks', 64916564355, 42);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Katelynn', 'Burch', 73879604222, 40);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Rocco', 'Sands-Cosgrove', 12265320583, 44);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Terence', 'Mcinnis', 51573595156, 40);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Mila', 'Overstreet', 74129295885, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Lory', 'Weeks', 44244356567, 38);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Stefania', 'Hamby', 21390964977, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Carlyn', 'Farrington', 96421769851, 45);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Meghann', 'Mata', 96024242958, 40);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Diann', 'New', 78760533860, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Idell', 'Peltier', 89024191711, 38);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Deidre', 'Rios', 67934802171, 43);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Nakia', 'Dallas', 49604364225, 43);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Carlene', 'Linton', 93138106452, 40);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Genesis', 'Brothers', 24872422015, 44);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Helena', 'Penrod', 90871849792, 40);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Angelina', 'Wyant', 10194538142, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Adrianna', 'Carmichael', 47041349066, 38);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Sandra', 'Main-Osgood', 11467922489, 45);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Billi', 'Ulrich', 12676496400, 40);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Gigi', 'Neel', 78719902614, 38);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Laila', 'Knott', 67751295244, 42);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Ta', 'Mcclure', 92011057859, 40);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Rayford', 'Pippin', 85125524716, 45);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Dona', 'Mccrory', 27419858012, 39);
+INSERT INTO people (f_name, s_name, phone_no, city_id)
+VALUES ('Wilford', 'Chisolm', 78146546175, 44);
