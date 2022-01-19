53778bb0fda10745b0192d0e3030f3c1557c6f20
karalkal <karalkal@yahoo.co.uk>
2022-01-19 01:16:19 +0000

    added capitals data

---
+++ people->cities->countries/01_INSERT_DATA/postgres_public_capitals.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (3, 'Berlin', 3220000.00, 3);
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (8, 'Madrid', 4330000.00, 6);
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (2, 'London', 11110000.00, 5);
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (7, 'Lisbon', 1100000.00, 11);
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (4, 'Rome', 2100000.00, 4);
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (1, 'Paris', 8030000.00, 2);
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (5, 'Warsaw', 1010000.00, 21);
+INSERT INTO public.capitals (capital_id, name, population, country_id) VALUES (6, 'Sofia', 1710000.00, 7);
