5dac077dac55f5dfc616575678848b910fb9f1f7
karalkal <karalkal@yahoo.co.uk>
2022-01-19 01:16:53 +0000

    added countries data

---
+++ people->cities->countries/01_INSERT_DATA/postgres_public_countries.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.countries (country_id, population, country_name) VALUES (3, 71.10, 'Germany');
+INSERT INTO public.countries (country_id, population, country_name) VALUES (2, 62.27, 'France');
+INSERT INTO public.countries (country_id, population, country_name) VALUES (5, 57.90, 'UK');
+INSERT INTO public.countries (country_id, population, country_name) VALUES (4, 51.51, 'Italy');
+INSERT INTO public.countries (country_id, population, country_name) VALUES (6, 44.44, 'Spain');
+INSERT INTO public.countries (country_id, population, country_name) VALUES (7, 7.33, 'Bulgaria');
+INSERT INTO public.countries (country_id, population, country_name) VALUES (11, 12.56, 'Portugal');
+INSERT INTO public.countries (country_id, population, country_name) VALUES (21, 22.22, 'Poland');
