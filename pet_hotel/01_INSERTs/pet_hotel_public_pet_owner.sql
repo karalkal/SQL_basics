d3194c0fbf8d58a059201f35cd131ca32dce1a33
karalkal <karalkal@yahoo.co.uk>
2022-01-20 07:11:25 +0000

    added table

---
+++ pet_hotel/01_INSERTs/pet_hotel_public_pet_owner.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.pet_owner (owner_id, owner_name, owner_age) VALUES (1, 'Peter', 26);
+INSERT INTO public.pet_owner (owner_id, owner_name, owner_age) VALUES (2, 'George', 32);
+INSERT INTO public.pet_owner (owner_id, owner_name, owner_age) VALUES (3, 'Amy', 67);
