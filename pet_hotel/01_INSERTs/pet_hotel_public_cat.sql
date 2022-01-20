3373cc1938851ecd31af5d987655dd6ae75d9d97
karalkal <karalkal@yahoo.co.uk>
2022-01-20 07:10:42 +0000

    added table

---
+++ pet_hotel/01_INSERTs/pet_hotel_public_cat.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.cat (cat_id, owner_id, cat_name, cat_age) VALUES (1, 2, 'Tommy', 1);
+INSERT INTO public.cat (cat_id, owner_id, cat_name, cat_age) VALUES (2, 3, 'Jessy', 7);
+INSERT INTO public.cat (cat_id, owner_id, cat_name, cat_age) VALUES (3, 2, 'Bubbles', 3);
