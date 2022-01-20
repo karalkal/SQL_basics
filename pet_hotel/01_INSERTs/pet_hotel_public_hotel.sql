6b8ef3a8987a004b45ded99b32f3c8a4e4e1667d
karalkal <karalkal@yahoo.co.uk>
2022-01-20 07:11:18 +0000

    added table

---
+++ pet_hotel/01_INSERTs/pet_hotel_public_hotel.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.hotel (hotel_id, hotel_name, hotel_stars) VALUES (1, 'Grand Pets Hotel', 5);
+INSERT INTO public.hotel (hotel_id, hotel_name, hotel_stars) VALUES (2, 'Pets Heaven', 2);
