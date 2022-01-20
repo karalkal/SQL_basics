6b0cd03d4cf4a543d14df788f90213814628e3cd
karalkal <karalkal@yahoo.co.uk>
2022-01-20 07:10:51 +0000

    added table

---
+++ pet_hotel/01_INSERTs/pet_hotel_public_dog_room.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.dog_room (room_id, dog_id, hotel_id, register_date, unregister_date) VALUES (1, 1, 1, '2020-06-08', '2020-06-10');
+INSERT INTO public.dog_room (room_id, dog_id, hotel_id, register_date, unregister_date) VALUES (2, 2, 2, '2020-06-10', '2020-06-15');
+INSERT INTO public.dog_room (room_id, dog_id, hotel_id, register_date, unregister_date) VALUES (3, 3, 2, '2020-06-20', '2020-06-23');
